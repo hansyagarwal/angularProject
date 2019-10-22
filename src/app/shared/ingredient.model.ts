6c1c0ec4d5d12a2a12c33084902f0a5f7a89731c
aryanag <hansy.agarwal@gmail.com>
2019-10-22 11:12:44 +0530

    first

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient{
+    /* alternative way
+    public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    }
+*/
+    constructor(public name: string, public amount: number){ }
+}
